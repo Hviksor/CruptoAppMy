dd29f4b3b9fd81ff8790f36ff5d33fb7a21eb812
Ololo <vsvktor@gmail.com>
2022-09-10 17:11:55 +0300

    add land orientation screen

--- app/src/main/java/com/example/cruptoappmy/presentor/screeens/MainActivity.kt
+++ app/src/main/java/com/example/cruptoappmy/presentor/screeens/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
 import com.example.cruptoappmy.R
+import com.example.cruptoappmy.presentor.screeens.FirstFragment.Companion.FIRST_FRAGMENT_NAME
 
 class MainActivity : AppCompatActivity() {
     lateinit var navController: NavController
@@ -12,9 +13,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        supportFragmentManager.popBackStack()
         supportFragmentManager.beginTransaction()
             .replace(R.id.nav_container_first, FirstFragment.getFragment())
-            .addToBackStack(null)
             .commit()
     }
+
+
 }
